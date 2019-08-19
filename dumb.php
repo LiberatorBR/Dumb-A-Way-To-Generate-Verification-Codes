6ffd0e94ad67df59ff07bd6b069b551474803a7b
LiberatorBR <liberator.or@gmail.com>
2019-08-19 08:44:04 -0400

    Update dumb.php

--- dumb.php
+++ dumb.php
@@ -1,25 +1,7 @@
 <?php
-function randomCode() {
-  function randL() {
-    $r = rand(1,3);
-    return $r;
-  }
-  function randH() {
-    $r = rand(3,5);
-    return $r;
-  }
-  $a = chr(rand(65,90));
-  $b = substr(md5(rand()), randL(), randH());
-  $c = chr(rand(65,90));
-  $d = substr(md5(rand()), randL(), randH());
-
-  $rand = $a.''.$b.''.$c.''.$d;
-  return $rand;
-}
 if($_SERVER['REQUEST_METHOD'] == "POST") {
-  $msg = randomCode();
+  $msg = chr(rand(65,90)).''.strtolower(substr(md5(rand()), 1, 3)).''.strtoupper(substr(md5(rand()), 1, 3));
 }
-
 ?>
 <html>
 <form action="" method="post">
